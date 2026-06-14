401f9c079713816d1c1b3a4147babd9e449da675
fagunyi-gif <fagunyi@gmail.com>
2026-06-14 15:21:04 -0300

    ELIMINÉ ImprimirTESTING
    totalmente eliminado, use su codigo en cada lugar que pedia imprimir. en consigna 2 y consigna 3

--- Ejercicios/Consigna_3/consigna_3.c
+++ Ejercicios/Consigna_3/consigna_3.c
@@ -23,11 +23,29 @@ for (int i = 0; i < 5; i++) {
     Apilar(&pila2, valor);
 }
 printf("Pila 1: ");
-ImprimirTESTING(pila1);
-printf("Pila 2: ");
-ImprimirTESTING(pila2);
 
-printf("ahora realizaremos la copia entre los elementos de la pila 1 y pila 2");
+printf("[");
+    while (!PilaVacia(pila1)) {
+        printf("%d", pila1->info);
+        if (pila1->sig != NULL) {
+            printf(", ");
+        }
+        pila1 = pila1->sig;
+    }
+    printf("]\n");
+
+printf("Pila 2: ");
+printf("[");
+    while (!PilaVacia(pila2)) {
+        printf("%d", pila2->info);
+        if (pila2->sig != NULL) {
+            printf(", ");
+        }
+        pila2 = pila2->sig;
+    }
+    printf("]\n");
+
+printf("ahora realizaremos la copia entre los elementos de la pila 1 y pila 2 ");
 while (!PilaVacia(pila1) && !PilaVacia(pila2)) {
     Desapilar(&pila1, &auxp1);
     Desapilar(&pila2, &auxp2);
@@ -36,5 +54,13 @@ while (!PilaVacia(pila1) && !PilaVacia(pila2)) {
 }
 printf("Pila de suma: ");
 
-ImprimirTESTING(resultadoSuma);
+printf ("[");
+    while (!PilaVacia(resultadoSuma)) {
+        printf("%d", resultadoSuma->info);
+        if (resultadoSuma->sig != NULL) {
+            printf(", ");
+        }
+        resultadoSuma = resultadoSuma->sig;
+    }
+    printf("]\n");
 }
